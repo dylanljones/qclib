cdf35105c6f6bbe6847f6ca554e867dbda287ae2
dylanljones <dylanljones94@gmail.com>
2021-10-06 10:38:55 +0200

    added matrix methods

--- qclib/math.py
+++ qclib/math.py
@@ -5,6 +5,8 @@
 # Copyright (c) 2021, Dylan Jones
 
 import numpy as np
+from scipy import linalg as la
+from functools import partial
 from typing import Optional, Iterable, Union
 
 # Pauli matrices
@@ -27,6 +29,8 @@ STATES = {"0": ZERO, "1": ONE, "+": PLUS, "-": MINUS, "i+": IPLUS, "i-": IMINUS}
 P0 = np.dot(ZERO[:, np.newaxis], ZERO[np.newaxis, :])
 P1 = np.dot(ONE[:, np.newaxis], ONE[np.newaxis, :])
 PROJECTIONS = [P0, P1]
+
+transpose = partial(np.swapaxes, axis1=-2, axis2=-1)
 
 
 def abs2(arr) -> np.ndarray:
@@ -254,6 +258,231 @@ def expectation(o: np.ndarray, psi: np.ndarray) -> np.ndarray:
     x : float
     """
     return np.dot(np.conj(psi).T, o.dot(psi)).real
+
+
+# =========================================================================
+
+
+def hermitian(a):
+    """Returns the hermitian of an array.
+
+    Parameters
+    ----------
+    a : array_like
+        The input array.
+
+    Returns
+    -------
+    hermitian : np.ndarray
+    """
+    return np.conj(np.transpose(a))
+
+
+def is_hermitian(a, rtol=1e-5, atol=1e-8, equal_nan=False):
+    """Checks if an array is hermitian.
+
+    Checks if the hermitian of the given array equals the array within
+    a tolerance by calling np.allcloe.
+
+    Parameters
+    ----------
+    a : array_like
+        The array to check.
+    rtol : float, optional
+        The relative tolerance parameter used in the comparison (see np.allclose).
+    atol : float, optional
+        The absolute tolerance parameter used in the comparison (see np.allclose).
+    equal_nan : bool, optional
+        Flag if ``NaN``'s are compared.
+
+    Returns
+    -------
+    hermitian : bool
+    """
+    return np.allclose(a, np.conj(np.transpose(a)), rtol, atol, equal_nan)
+
+
+def fill_diagonal(mat, val, offset=0):
+    """Fills the optionally offset diagonal of an array.
+
+    Parameters
+    ----------
+    mat : np.ndarray
+        Array whose diagonal is to be filled, it gets modified in-place.
+    val : scalar or array_like
+        Value to be written on the diagonal, its type must be compatible
+        with that of the array a.
+    offset : int, optional
+        offset index of diagonal. An offset of 1 results
+        in the first upper offdiagonal of the matrix,
+        an offset of -1 in the first lower offdiagonal.
+    """
+    if offset:
+        n, m = mat.shape
+        if offset > 0:
+            # Upper off-diagonal
+            idx = slice(0, n), slice(abs(offset), m)
+        else:
+            # Lower off-diagonal
+            idx = slice(abs(offset), n), slice(0, m)
+        np.fill_diagonal(mat[idx], val)
+
+    else:
+        np.fill_diagonal(mat, val)
+
+
+def decompose(arr, h=None):
+    """Computes the eigen-decomposition of a matrix.
+
+    Finds the eigenvalues and the left and right eigenvectors of a matrix. If the matrix
+    is hermitian ``eigh`` is used for computing the right eigenvectors.
+
+    Parameters
+    ----------
+    arr : (N, N) array_like
+        A complex or real matrix whose eigenvalues and eigenvectors will be computed.
+    h : bool, optional
+        Flag if matrix is hermitian. If ``None`` the matrix is checked. This determines the
+        scipy method used for computing the eigenvalue problem.
+
+    Returns
+    -------
+    vr : (N, N) np.ndarray
+        The right eigenvectors of the matrix.
+    xi : (N, ) np.ndarray
+        The eigenvalues of the matrix.
+    vl : (N, N) np.ndarray
+        The left eigenvectors of the matrix.
+    """
+    if h is None:
+        h = is_hermitian(arr)
+    if h:
+        xi, vr = np.linalg.eigh(arr)
+        vl = np.conj(vr.T)
+    else:
+        xi, vl, vr = la.eig(arr, left=True, right=True)
+    return vr, xi, vl
+
+
+def reconstruct(rv, xi, rv_inv, method='full'):
+    """Computes a matrix from an eigen-decomposition.
+
+    The matrix is reconstructed using eigenvalues and left and right eigenvectors.
+
+    Parameters
+    ----------
+    rv : (N, N) np.ndarray
+        The right eigenvectors of a matrix.
+    xi : (N, ) np.ndarray
+        The eigenvalues of a matrix
+    rv_inv : (N, N) np.ndarray
+        The left eigenvectors of a matrix.
+    method : str, optional
+        The mode for reconstructing the matrix. If mode is 'full' the original matrix
+        is reconstructed, if mode is 'diag' only the diagonal elements are computed.
+        The default is 'full'.
+
+    Returns
+    -------
+    arr : (N, N) np.ndarray
+        The reconstructed matrix.
+    """
+    method = method.lower()
+    if 'diag'.startswith(method):
+        arr = ((transpose(rv_inv) * rv) @ xi[..., np.newaxis])[..., 0]
+    elif 'full'.startswith(method):
+        arr = (rv * xi[..., np.newaxis, :]) @ rv_inv
+    else:
+        arr = np.einsum(method, rv, xi, rv_inv)
+    return arr
+
+
+class Decomposition:
+    """Eigen-decomposition of a matrix.
+
+    Parameters
+    ----------
+    rv : (N, N) np.ndarray
+        The right eigenvectors of a matrix.
+    xi : (N, ) np.ndarray
+        The eigenvalues of a matrix
+    rv_inv : (N, N) np.ndarray
+        The left eigenvectors of a matrix.
+    """
+    __slots__ = ('rv', 'xi', 'rv_inv')
+
+    def __init__(self, rv, xi, rv_inv):
+        self.rv = rv
+        self.xi = xi
+        self.rv_inv = rv_inv
+
+    @classmethod
+    def decompose(cls, arr, h=None):
+        """Computes the decomposition of a matrix and initializes a ``Decomposition``-instance.
+
+        Parameters
+        ----------
+        arr : (N, N) array_like
+            A complex or real matrix whose eigenvalues and eigenvectors will be computed.
+        h : bool, optional
+            Flag if matrix is hermitian. If ``None`` the matrix is checked. This determines the
+            scipy method used for computing the eigenvalue problem.
+
+        Returns
+        -------
+        decomposition : Decomposition
+        """
+        rv, xi, rv_inv = decompose(arr, h)
+        return cls(rv, xi, rv_inv)
+
+    def reconstrunct(self, xi=None, method='full'):
+        """Computes a matrix from the eigen-decomposition.
+
+        Parameters
+        ----------
+        xi : (N, ) array_like, optional
+            Optional eigenvalues to compute the matrix. If ``None`` the eigenvalues of the
+            decomposition are used. The default is ``None``.
+        method : str, optional
+        The mode for reconstructing the matrix. If mode is 'full' the original matrix
+        is reconstructed, if mode is 'diag' only the diagonal elements are computed.
+        The default is 'full'.
+
+        Returns
+        -------
+        arr : (N, N) np.ndarray
+            The reconstructed matrix.
+        """
+        xi = self.xi if xi is None else xi
+        return reconstruct(self.rv, xi, self.rv_inv, method)
+
+    def normalize(self):
+        """Normalizes the eigenstates and creates a new ``Decomposition``-instance."""
+        rv = self.rv / np.linalg.norm(self.rv)
+        rv_inv = self.rv_inv / np.linalg.norm(self.rv_inv)
+        return self.__class__(rv, self.xi, rv_inv)
+
+    def transform_basis(self, transformation):
+        """Transforms the eigen-basis into another basis.
+
+        Parameters
+        ----------
+        transformation : (N, N) array_like
+            The transformation-matrix to transform the basis.
+        """
+        rv = self.rv @ np.asarray(transformation)
+        self.rv = rv
+        self.rv_inv = np.linalg.inv(rv)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}[{self.rv.shape}x{self.xi.shape}x{self.rv_inv.shape}]"
+
+
+def get_ground_state(eigvals, eigvecs):
+    idx = np.argmin(eigvals)
+    gs = eigvecs[:, idx]
+    gs_energy = eigvals[idx]
+    return gs_energy, gs
 
 
 # =========================================================================
